3f6a8dd36be5c4fcab6321d90e07934d9adfffe1
sammy180 <sammyhassan95@gmail.com>
2020-11-25 12:13:50 +0000

    Initial commit

---
+++ EMS/ems home/views/logged_in.php
@@ -0,0 +1,17 @@
+<!-- if you need user information, just put them into the $_SESSION variable and output them here -->
+Hey, <?php echo $_SESSION['user_name']; ?>. You are logged in.
+Try to close this browser tab and open it again. Still logged in! ;)
+
+
+<div class="sites">
+                <ul>
+                    <li> site a data </li>
+                    <!-- <li><a  href="EMS/ems/home/index.php"> Machine learning projects   </a></li>    -->
+                    <li> site b data </a></li>
+                    <li> site c data </li>
+                    <li> site d data </li>
+                </ul>
+            </div>
+
+<!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
+<bl></bl> <a href="index.php?logout">Logout</a>
